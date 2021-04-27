5e6d8a344ec82a0d70e65d610bd8daf03fb8e658
BoyuanWang0421 <71746234+BoyuanWang0421@users.noreply.github.com>
2021-04-27 16:37:17 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -3,15 +3,13 @@ import re
 
 word = re.compile(r'(\w*herit\w*)', re.IGNORECASE)
 
-text=''
 file = open('origin.txt')
 for line in file:
-    new_file=open('result.txt','w')
-    line_number=0
-    line_number+=1
-    if word.search(line):
-        new_file.write(str(line_number)+"\t"+word.search(line).group(0)+"\n")
-new_file.close()
+    with open('result.txt','w') as new_file:
+        line_number=0
+        line_number+=1
+        if word.search(line):
+            new_file.write(str(line_number)+"\t"+word.search(line).group(0)+"\n")
 
 
 
